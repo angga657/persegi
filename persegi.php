ff7a0cdb702c39a1fdde4299c80f09a4bc01fef1
angga657 <117795076+angga657@users.noreply.github.com>
2023-11-14 17:57:06 +0800

    variable, function, class

--- persegi.php
+++ persegi.php
@@ -4,3 +4,32 @@ $sisi = 6;
 $luas = $sisi * $sisi;
 
 echo "Hasil Dari Perhitungan Luas Persegi Adalah = $luas <br>";
+
+function persegi($sisi)
+ { 
+
+     $luas = ($sisi * $sisi);
+     return $luas;
+ }
+
+ $fungsi_persegi = persegi($sisi);
+
+ echo "Hasil Dari Perhitungan Luas Persegi Menggunakan Function Adalah = $fungsi_persegi <br>";
+
+
+ class persegi
+ {
+     function hitung_luas($sisi)
+     {
+         $luas = $sisi * $sisi;
+         return $luas;
+     }
+ }
+
+ $kotak = new persegi();
+
+
+ echo "Hasil Perhitungan Luas = ";
+ echo $kotak->hitung_luas($sisi);
+
+
